c86fa531d58c2361ff6534937febbfb13f688175
onlybenyang <thomas199388@163.com>
2024-11-29 11:56:33 +0800

    base on 0.76.3 RN and config project can build sucess

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+/**
+ * 普通的按钮
+ */
+import React from "react";
+import { Button as RNButton } from "react-native";
+
+export default function Button() {
+    return <RNButton title="Click me" />;
+}
